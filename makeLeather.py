80f8be0df7b6052defb692e336bd697b0444094c
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2020-01-16 18:37:11 -0600

    Add files via upload

--- makeLeather.py
+++ makeLeather.py
@@ -8,6 +8,8 @@ import decimal
 hc = HumanClicker()
 iterations = 600 # how many times the script should loop and cast make leather
 
+# initialize function to generate random time dleay
+
 for i in range(iterations):
     # establish random coordinates to avoid bot detection
     pos = random.randrange(-50,50,1)
@@ -19,44 +21,28 @@ for i in range(iterations):
     depositver = random.randrange(-30,30,1)
 
     # establish random times to avoid bot detection
-    spelltime = random.randrange(0,1,1)
-    banktime = random.randrange(0,1,1)
-    allothertime = random.randrange(0,2,1)
+    spellTime = random.randrange(0,1,1)
+    bankTime = random.randrange(0,1,1)
+    allOtherTime = random.randrange(0,2,1)
+   # movementTime = 
 
     # banking
-    hc.move((929+pos,459+pos),3+spelltime)
+    hc.move((929+pos,459+pos),3+spellTime)
     hc.click()
-    hc.move((1113+deposithor,659+depositver),1+banktime)
+    hc.move((1113+deposithor,659+depositver),1+bankTime)
     hc.click()
    
     # withdraw 25 black dragonhide
-    hc.move((827+dhidehor,377+dhidever),1+allothertime)
+    hc.move((827+dhidehor,377+dhidever),1+allOtherTime)
     hc.click()
     time.sleep(float(random.randrange(52, 120))/100)
     pyautogui.press('esc')
 
     # casting the spell
-    hc.move((1512+spellhor,682+spellver),1+allothertime)
+    hc.move((1512+spellhor,682+spellver),1+allOtherTime)
     hc.click()
-    time.sleep(float(random.randrange(52, 120))/100)
-    hc.click()
-    time.sleep(float(random.randrange(52, 120))/100)
-    hc.click()
-    time.sleep(float(random.randrange(52, 120))/100)
-    hc.click()
-    time.sleep(float(random.randrange(52, 120))/100)
-    hc.click()
-    time.sleep(float(random.randrange(52, 120))/100)
-    hc.click()
-    time.sleep(float(random.randrange(52, 120))/100)
-    hc.click()
-    time.sleep(float(random.randrange(52, 120))/100)
-    hc.click()
-    time.sleep(float(random.randrange(52, 120))/100)
-    hc.click()
-    time.sleep(float(random.randrange(52, 120))/100)
-    hc.click()
-    time.sleep(float(random.randrange(52, 120))/100)
-    hc.click()
+    for i in range(10):
+        time.sleep(float(random.randrange(52, 120))/100)
+        hc.click()
     time.sleep(float(random.randrange(52, 120))/100)
     
